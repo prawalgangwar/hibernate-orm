6dabb7072047089aa1995855a5c3bd1ef2a6ca8a
Prawal Gangwar <prawal.gangwar@gmail.com>
2019-06-04 10:45:10 +0000

    renamed the files with unicode characters to fix locale issue

--- hibernate-core/src/test/java/org/hibernate/internal/util/ReflectHelperTest.java
+++ hibernate-core/src/test/java/org/hibernate/internal/util/ReflectHelperTest.java
@@ -13,7 +13,7 @@ import javax.persistence.FetchType;
 import org.hibernate.boot.registry.classloading.spi.ClassLoaderService;
 import org.hibernate.boot.spi.SessionFactoryOptions;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
-import org.hibernate.internal.util.hib3rnat3.C0nst4nts३;
+import org.hibernate.internal.util.hib3rnat3.C0nst4nts;
 import org.hibernate.service.spi.ServiceRegistryImplementor;
 
 import org.hibernate.testing.TestForIssue;
@@ -191,10 +191,10 @@ public class ReflectHelperTest {
 	public void test_getConstantValue_constant_digits() {
 
 		when( sessionFactoryOptionsMock.isConventionalJavaConstants() ).thenReturn( true );
-		when( classLoaderServiceMock.classForName( "org.hibernate.internal.util.hib3rnat3.C0nst4nts३" ) ).thenReturn( (Class) C0nst4nts३.class );
-		Object value = ReflectHelper.getConstantValue( "org.hibernate.internal.util.hib3rnat3.C0nst4nts३.ABC_DEF", sessionFactoryImplementorMock);
-		assertEquals( C0nst4nts३.ABC_DEF, value );
-		verify(classLoaderServiceMock, times(1)).classForName( eq("org.hibernate.internal.util.hib3rnat3.C0nst4nts३") );
+		when( classLoaderServiceMock.classForName( "org.hibernate.internal.util.hib3rnat3.C0nst4nts" ) ).thenReturn( (Class) C0nst4nts.class );
+		Object value = ReflectHelper.getConstantValue( "org.hibernate.internal.util.hib3rnat3.C0nst4nts.ABC_DEF", sessionFactoryImplementorMock);
+		assertEquals( C0nst4nts.ABC_DEF, value );
+		verify(classLoaderServiceMock, times(1)).classForName( eq("org.hibernate.internal.util.hib3rnat3.C0nst4nts") );
 	}
 
 	@Test
--- hibernate-core/src/test/java/org/hibernate/internal/util/hib3rnat3/C0nst4nts.java
+++ hibernate-core/src/test/java/org/hibernate/internal/util/hib3rnat3/C0nst4nts.java
@@ -9,7 +9,7 @@ package org.hibernate.internal.util.hib3rnat3;
 /**
  * @author Vlad Mihalcea
  */
-public class C0nst4nts३ {
+public class C0nst4nts {
 
 	public static final String ABC_DEF = "xyz";
 }
